12a3a2fb3c443495230f86dfced90ea823b7e608
bimal-bp <bimalpatrap@gmail.com>
2024-03-16 16:24:33 +0530

    Data validation done

--- src/constant/training_pipilien/__init__.py
+++ src/constant/training_pipilien/__init__.py
@@ -6,8 +6,7 @@ PIPELINE_NAME: str = "sensor"
 ARTIFACT_DIR: str = "artifact"
 FILE_NAME: str = "sensor.csv"
 
-TRAIN_FILE_NAME: str = "train.csv"
-TEST_FILE_NAME: str = "test.csv"
+
 # Data ingestion part 
 DATA_INGESTION_COLLECTION_NAME: str = "sensor"
 DATA_INGESTION_DIR_NAME:str ="data_ingestion"
@@ -17,3 +16,16 @@ DATA_INGESTION_TRAIN_TEST_SPLIT_RATION:float = 0.2
 
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 SCHEMA_DROP_COLS = "drop_columns"
+
+# Data validation 
+
+DATA_VALIDATION_DIR_NAME: str = "data_validtion"
+DATA_VALIDATION_VALID_DIR: str="validated"
+DATA_VALIDATON_INVALID_DIR:str= "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str  = "drift_report"
+DATA_VALIDATION_DRIFT_FILE_NAME:str ="report.yaml"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+
+
--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -1,9 +1,10 @@
-from src.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
-from src.entity.artifact_entity import DataIngestionArtifact
+from src.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig
+from src.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
 import os ,sys
 from src.utils.logger import logging
 from src.utils.exception import customexception 
 from src.components.data_ingestion import DataIngestion 
+from src.components.data_validation import DataValidation
 
 
 class TrainPipeline:
@@ -22,12 +23,21 @@ class TrainPipeline:
         except Exception as e:
             raise customexception(e,sys)
 
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        try:
+            data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                         data_validation_config=data_validation_config)
+            data_validation_artifact = data_validation.initiate_data_validation()
+            return data_validation_artifact
+        except Exception as e:
+            raise customexception(e, sys)
+
 
     def run_pipeline(self):
         try:
             TrainPipeline.is_pipeline_running=True
-
             data_ingestion_artifact:DataIngestionArtifact=self.start_data_ingestion()
-
-        except Exception as e:
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+        except Exception as e :
             raise customexception(e,sys)
